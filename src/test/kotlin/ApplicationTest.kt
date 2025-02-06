22c276aab974bbd2feec6f2a8ed4e75abaf66147
mehrbod <mehrbod.kh@gmail.com>
2025-02-06 23:48:15 +0100

    Add tests

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -15,7 +15,7 @@ import kotlin.test.assertEquals
 class ApplicationTest {
 
     @Test
-    fun testRoot() = testApplication {
+    fun `should add talk`() = testApplication {
         environment {
             config = ApplicationConfig("application-testing.yaml")
         }
@@ -35,6 +35,52 @@ class ApplicationTest {
         assertEquals(getSampleData(), response.body())
     }
 
+    @Test
+    fun `should remove talk`() = testApplication {
+        environment {
+            config = ApplicationConfig("application-testing.yaml")
+        }
+        val client = createClient {
+            this.install(ContentNegotiation) {
+                json()
+            }
+        }
+        getSampleData().forEach {
+            client.post("/talks/add") {
+                header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                setBody(it)
+            }
+        }
+        client.post("/talks/remove") {
+            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            setBody(getSampleData().first())
+        }
+        val response = client.get("/talks")
+
+        assertEquals(getSampleData().subList(1, 3), response.body())
+    }
+
+    @Test
+    fun `should get most influential talk`() = testApplication {
+        environment {
+            config = ApplicationConfig("application-testing.yaml")
+        }
+        val client = createClient {
+            this.install(ContentNegotiation) {
+                json()
+            }
+        }
+        getSampleData().forEach {
+            client.post("/talks/add") {
+                header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                setBody(it)
+            }
+        }
+        val response = client.get("/top_talks")
+
+        assertEquals(getSampleData().reversed(), response.body())
+    }
+
     fun getSampleData() = listOf<TedTalk>(
         TedTalk(
             "sample_title",
@@ -47,7 +93,7 @@ class ApplicationTest {
         TedTalk(
             "sample_title_2",
             "sample_author_2",
-            "12-11-2011",
+            "12-11-2013",
             12000L,
             12000L,
             "https://sample_link.com",
@@ -55,7 +101,7 @@ class ApplicationTest {
         TedTalk(
             "sample_title_3",
             "sample_author_3",
-            "13-11-2011",
+            "13-11-2013",
             14000L,
             12000L,
             "https://sample_link.com",
