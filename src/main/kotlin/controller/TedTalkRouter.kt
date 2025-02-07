4d30f71de6ce155c91c08784428b7ec805e4b25a
mehrbod <mehrbod.kh@gmail.com>
2025-02-07 07:06:02 +0100

    Add open API specification

--- src/main/kotlin/controller/TedTalkRouter.kt
+++ src/main/kotlin/controller/TedTalkRouter.kt
@@ -2,9 +2,10 @@ package com.mehrbod.controller
 
 import com.mehrbod.domain.TedTalkService
 import com.mehrbod.domain.model.TedTalk
-import io.ktor.server.application.Application
-import io.ktor.server.request.receive
-import io.ktor.server.response.respond
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 
@@ -13,17 +14,19 @@ fun Application.tedTalkRouter() {
     routing {
         route("/talks") {
             get {
-                val authorName = call.queryParameters["name"]
+                val authorName = call.queryParameters["author"]
                 val result = service.getTedTalks(authorName)
                 call.respond(result)
             }
             post("/add") {
                 val input: TedTalk = call.receive()
                 service.addTedTalk(input)
+                call.respond(HttpStatusCode.Created, "Created")
             }
             post("/remove") {
                 val input: TedTalk = call.receive()
                 service.removeTedTalk(input)
+                call.respond(HttpStatusCode.OK, "Deleted")
             }
             get("/top_talks") {
                 val number = call.queryParameters["n"]?.toInt() ?: 10
